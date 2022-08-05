fcc023059e6cf3b2e949dfb9130a3855b0783189
John Turpish <john@littlebearlabs.io>
2022-08-05 01:26:58 -0400

    Allow the caller to set the prologue.

--- src/noise.ts
+++ src/noise.ts
@@ -29,29 +29,40 @@ interface HandshakeParams {
 }
 
 export class Noise implements INoiseConnection {
-  public protocol = '/noise'
-  public crypto: ICryptoInterface
+  public protocol = "/noise";
+  public crypto: ICryptoInterface;
 
-  private readonly prologue = new Uint8Array(0)
-  private readonly staticKeys: KeyPair
-  private readonly earlyData?: bytes
-  private readonly useNoisePipes: boolean
+  private readonly prologue: Uint8Array;
+  private readonly staticKeys: KeyPair;
+  private readonly earlyData?: bytes;
+  private readonly useNoisePipes: boolean;
 
   /**
    * @param {bytes} staticNoiseKey - x25519 private key, reuse for faster handshakes
    * @param {bytes} earlyData
    */
-  constructor (staticNoiseKey?: bytes, earlyData?: bytes, crypto: ICryptoInterface = stablelib) {
-    this.earlyData = earlyData ?? new Uint8Array(0)
+  constructor(
+    staticNoiseKey?: bytes,
+    earlyData?: bytes,
+    crypto: ICryptoInterface = stablelib,
+    prologueBytes?: Uint8Array
+  ) {
+    this.earlyData = earlyData ?? new Uint8Array(0);
     // disabled until properly specked
-    this.useNoisePipes = false
-    this.crypto = crypto
+    this.useNoisePipes = false;
+    this.crypto = crypto;
 
     if (staticNoiseKey) {
       // accepts x25519 private key of length 32
-      this.staticKeys = this.crypto.generateX25519KeyPairFromSeed(staticNoiseKey)
+      this.staticKeys =
+        this.crypto.generateX25519KeyPairFromSeed(staticNoiseKey);
     } else {
-      this.staticKeys = this.crypto.generateX25519KeyPair()
+      this.staticKeys = this.crypto.generateX25519KeyPair();
+    }
+    if (prologueBytes) {
+      this.prologue = prologueBytes;
+    } else {
+      this.prologue = new Uint8Array(0);
     }
   }
 
@@ -63,28 +74,32 @@ export class Noise implements INoiseConnection {
    * @param {PeerId} remotePeer - PeerId of the remote peer. Used to validate the integrity of the remote peer.
    * @returns {Promise<SecuredConnection>}
    */
-  public async secureOutbound (localPeer: PeerId, connection: any, remotePeer: PeerId): Promise<SecuredConnection> {
-    const wrappedConnection = pbStream(
-      connection,
-      {
-        lengthEncoder: uint16BEEncode,
-        lengthDecoder: uint16BEDecode,
-        maxDataLength: NOISE_MSG_MAX_LENGTH_BYTES
-      }
-    )
+  public async secureOutbound(
+    localPeer: PeerId,
+    connection: Duplex<Uint8Array>,
+    remotePeer: PeerId
+  ): Promise<SecuredConnection> {
+    const wrappedConnection = pbStream(connection, {
+      lengthEncoder: uint16BEEncode,
+      lengthDecoder: uint16BEDecode,
+      maxDataLength: NOISE_MSG_MAX_LENGTH_BYTES,
+    });
     const handshake = await this.performHandshake({
       connection: wrappedConnection,
       isInitiator: true,
       localPeer,
-      remotePeer
-    })
-    const conn = await this.createSecureConnection(wrappedConnection, handshake)
+      remotePeer,
+    });
+    const conn = await this.createSecureConnection(
+      wrappedConnection,
+      handshake
+    );
 
     return {
       conn,
       remoteEarlyData: handshake.remoteEarlyData,
-      remotePeer: handshake.remotePeer
-    }
+      remotePeer: handshake.remotePeer,
+    };
   }
 
   /**
@@ -95,28 +110,32 @@ export class Noise implements INoiseConnection {
    * @param {PeerId} remotePeer - optional PeerId of the initiating peer, if known. This may only exist during transport upgrades.
    * @returns {Promise<SecuredConnection>}
    */
-  public async secureInbound (localPeer: PeerId, connection: any, remotePeer?: PeerId): Promise<SecuredConnection> {
-    const wrappedConnection = pbStream(
-      connection,
-      {
-        lengthEncoder: uint16BEEncode,
-        lengthDecoder: uint16BEDecode,
-        maxDataLength: NOISE_MSG_MAX_LENGTH_BYTES
-      }
-    )
+  public async secureInbound(
+    localPeer: PeerId,
+    connection: any,
+    remotePeer?: PeerId
+  ): Promise<SecuredConnection> {
+    const wrappedConnection = pbStream(connection, {
+      lengthEncoder: uint16BEEncode,
+      lengthDecoder: uint16BEDecode,
+      maxDataLength: NOISE_MSG_MAX_LENGTH_BYTES,
+    });
     const handshake = await this.performHandshake({
       connection: wrappedConnection,
       isInitiator: false,
       localPeer,
-      remotePeer
-    })
-    const conn = await this.createSecureConnection(wrappedConnection, handshake)
+      remotePeer,
+    });
+    const conn = await this.createSecureConnection(
+      wrappedConnection,
+      handshake
+    );
 
     return {
       conn,
       remoteEarlyData: handshake.remoteEarlyData,
-      remotePeer: handshake.remotePeer
-    }
+      remotePeer: handshake.remotePeer,
+    };
   }
 
   /**
@@ -125,17 +144,21 @@ export class Noise implements INoiseConnection {
    *
    * @param {HandshakeParams} params
    */
-  private async performHandshake (params: HandshakeParams): Promise<IHandshake> {
-    const payload = await getPayload(params.localPeer, this.staticKeys.publicKey, this.earlyData)
-    let tryIK = this.useNoisePipes
+  private async performHandshake(params: HandshakeParams): Promise<IHandshake> {
+    const payload = await getPayload(
+      params.localPeer,
+      this.staticKeys.publicKey,
+      this.earlyData
+    );
+    let tryIK = this.useNoisePipes;
     if (params.isInitiator && KeyCache.load(params.remotePeer) === null) {
       // if we are initiator and remote static key is unknown, don't try IK
-      tryIK = false
+      tryIK = false;
     }
     // Try IK if acting as responder or initiator that has remote's static key.
     if (tryIK) {
       // Try IK first
-      const { remotePeer, connection, isInitiator } = params
+      const { remotePeer, connection, isInitiator } = params;
       const ikHandshake = new IKHandshake(
         isInitiator,
         payload,
@@ -146,65 +169,69 @@ export class Noise implements INoiseConnection {
         // safe to cast as we did checks
         KeyCache.load(params.remotePeer) ?? new Uint8Array(32),
         remotePeer as PeerId
-      )
+      );
 
       try {
-        return await this.performIKHandshake(ikHandshake)
+        return await this.performIKHandshake(ikHandshake);
       } catch (e) {
-        const err = e as FailedIKError
+        const err = e as FailedIKError;
 
         // IK failed, go to XX fallback
-        let ephemeralKeys
+        let ephemeralKeys;
         if (params.isInitiator) {
-          ephemeralKeys = ikHandshake.getLocalEphemeralKeys()
+          ephemeralKeys = ikHandshake.getLocalEphemeralKeys();
         }
-        return await this.performXXFallbackHandshake(params, payload, err.initialMsg as Uint8Array, ephemeralKeys)
+        return await this.performXXFallbackHandshake(
+          params,
+          payload,
+          err.initialMsg as Uint8Array,
+          ephemeralKeys
+        );
       }
     } else {
       // run XX handshake
-      return await this.performXXHandshake(params, payload)
+      return await this.performXXHandshake(params, payload);
     }
   }
 
-  private async performXXFallbackHandshake (
+  private async performXXFallbackHandshake(
     params: HandshakeParams,
     payload: bytes,
     initialMsg: bytes,
     ephemeralKeys?: KeyPair
   ): Promise<XXFallbackHandshake> {
-    const { isInitiator, remotePeer, connection } = params
-    const handshake =
-      new XXFallbackHandshake(
-        isInitiator,
-        payload,
-        this.prologue,
-        this.crypto,
-        this.staticKeys,
-        connection,
-        initialMsg,
-        remotePeer,
-        ephemeralKeys
-      )
+    const { isInitiator, remotePeer, connection } = params;
+    const handshake = new XXFallbackHandshake(
+      isInitiator,
+      payload,
+      this.prologue,
+      this.crypto,
+      this.staticKeys,
+      connection,
+      initialMsg,
+      remotePeer,
+      ephemeralKeys
+    );
 
     try {
-      await handshake.propose()
-      await handshake.exchange()
-      await handshake.finish()
+      await handshake.propose();
+      await handshake.exchange();
+      await handshake.finish();
     } catch (e) {
-      const err = e as Error
-      err.message = `Error occurred during XX Fallback handshake: ${err.message}`
-      logger(err)
-      throw err
+      const err = e as Error;
+      err.message = `Error occurred during XX Fallback handshake: ${err.message}`;
+      logger(err);
+      throw err;
     }
 
-    return handshake
+    return handshake;
   }
 
-  private async performXXHandshake (
+  private async performXXHandshake(
     params: HandshakeParams,
     payload: bytes
   ): Promise<XXHandshake> {
-    const { isInitiator, remotePeer, connection } = params
+    const { isInitiator, remotePeer, connection } = params;
     const handshake = new XXHandshake(
       isInitiator,
       payload,
@@ -213,42 +240,42 @@ export class Noise implements INoiseConnection {
       this.staticKeys,
       connection,
       remotePeer
-    )
+    );
 
     try {
-      await handshake.propose()
-      await handshake.exchange()
-      await handshake.finish()
+      await handshake.propose();
+      await handshake.exchange();
+      await handshake.finish();
 
       if (this.useNoisePipes && handshake.remotePeer) {
-        KeyCache.store(handshake.remotePeer, handshake.getRemoteStaticKey())
+        KeyCache.store(handshake.remotePeer, handshake.getRemoteStaticKey());
       }
     } catch (e: unknown) {
       if (e instanceof Error) {
-        e.message = `Error occurred during XX handshake: ${e.message}`
-        throw e
+        e.message = `Error occurred during XX handshake: ${e.message}`;
+        throw e;
       }
     }
 
-    return handshake
+    return handshake;
   }
 
-  private async performIKHandshake (
+  private async performIKHandshake(
     handshake: IKHandshake
   ): Promise<IKHandshake> {
-    await handshake.stage0()
-    await handshake.stage1()
+    await handshake.stage0();
+    await handshake.stage1();
 
-    return handshake
+    return handshake;
   }
 
-  private async createSecureConnection (
+  private async createSecureConnection(
     connection: ProtobufStream,
     handshake: IHandshake
   ): Promise<Duplex<Uint8Array>> {
     // Create encryption box/unbox wrapper
-    const [secure, user] = duplexPair<Uint8Array>()
-    const network = connection.unwrap()
+    const [secure, user] = duplexPair<Uint8Array>();
+    const network = connection.unwrap();
 
     await pipe(
       secure, // write to wrapper
@@ -258,8 +285,8 @@ export class Noise implements INoiseConnection {
       decode({ lengthDecoder: uint16BEDecode }), // read message length prefix
       decryptStream(handshake), // decrypt the incoming data
       secure // pipe to the wrapper
-    )
+    );
 
-    return user
+    return user;
   }
 }
